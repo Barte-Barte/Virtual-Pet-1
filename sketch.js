91dccf7ae645f84f3731ef42c8ce05b82b76e59a
Barte-Barte <dev.shah00404@gmail.com>
2020-10-22 22:44:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,83 +1,57 @@
-var dog, happyDog, database, foodS, foodStock;
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
 
-//img
-var dogImg;
+function preload(){
+   dogImg=loadImage("Dog.png");
+   dogImg1=loadImage("happydog.png");
+  }
 
-function preload()
-{
-  dogImg=loadImage('Dog.png')
-  happyDog=loadImage('happydog.png')
-}
-
+//Function to set initial environment
 function setup() {
-	createCanvas(500, 500);
-  background(46, 139, 87)
+  database=firebase.database();
+  createCanvas(500,500);
 
-dog =createSprite(250,250,30,30);
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
 
-dog.scale=0.2
-
-database = firebase.database();
-
-foodStock= database.ref("Food");
-foodStock.on("value", readStock, error)
-
-
-
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
 }
 
-
-function draw() {  
-
-if(keyDown(UP_ARROW))
-{
-
-writeStock(foodS);
-dog.addImage(happyDog);
-
-}
-else{dog.addImage(dogImg)}
-
-
-
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
 
   drawSprites();
-  textSize(20);
-  fill("blue")
-  text("Note: Press the UP_ARROW To Feed the Dog Milk",10,100)
-
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed the Dog Milk!",130,10,300,20);
 }
 
-
-
-function error()
-
-{
-
-
-console.log("ERROR!!!!!!")
-
-
-}
-
+//Function to read values from DB
 function readStock(data){
-
   foodS=data.val();
 }
 
+//Function to write values in DB
 function writeStock(x){
-
-if(x<=0){
-  x=0;
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
 }
-else{
-
-  x=x-1
-}
-database.ref('/').update({
-  Food:x
-})
-
-}
-
-
+  
